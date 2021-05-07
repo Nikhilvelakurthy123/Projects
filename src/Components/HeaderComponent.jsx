81bdafa774fc1aa0514fcfbe09b0df96476d19b9
nikhil <bunnynik6@gmail.com>
2021-05-08 00:57:39 +0530

    home page

--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -9,7 +9,7 @@ export default class HeaderComponent extends Component {
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                         <div>
-                            <a href="www.google.com" className="navbar-brand">Calculator</a>
+                            <a href="/" className="navbar-brand">Calculator</a>
                         </div>
                         <ul className="navbar-nav navbar-collapse justify-content-end">
                             <Link className="nav-link" to="/">Home</Link>
